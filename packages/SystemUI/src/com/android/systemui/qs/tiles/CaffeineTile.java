c6a2e558dabf21d1901759c043b2fb789a3dce35
micky387 <mickaelsaibi@free.fr>
2024-09-12 23:00:26 -0400

    base: Update to QS Tiles with Expandable animation
    After https://gitlab.com/omnirom/android_frameworks_base/-/commit/0acf092a2e582ed08aa2d0261f87eac1091e4636
    
    Change-Id: I81a46b20db6109dabd3692ade2730ddb5713cb19

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -27,10 +27,10 @@ import android.os.Looper;
 import android.os.PowerManager;
 import android.os.SystemClock;
 import android.service.quicksettings.Tile;
-import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import com.android.systemui.animation.Expandable;
 import com.android.internal.logging.MetricsLogger;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
@@ -107,7 +107,7 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    protected void handleClick(@Nullable View view) {
+    protected void handleClick(@Nullable Expandable expandable) {
         // If last user clicks < 5 seconds
         // we cycle different duration
         // otherwise toggle on/off
@@ -145,7 +145,7 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    protected void handleLongClick(@Nullable View view) {
+    protected void handleLongClick(@Nullable Expandable expandable) {
         if (mWakeLock.isHeld()) {
             if (mDuration == INFINITE_DURATION_INDEX) {
                 return;
