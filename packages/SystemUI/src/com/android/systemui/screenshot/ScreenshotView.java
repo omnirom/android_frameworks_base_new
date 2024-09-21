omnirom/android_frameworks_base_old

SystemUI: add resource bool to force show screenshot dismiss button
by default it is only shown with enabled accessibility

Change-Id: I492262a14ed7bf4465326f158d6679d632cc208a

packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotView.java
@@ -176,6 +176,8 @@ public class ScreenshotView extends FrameLayout implements
         QUICK_SHARE
     }
 
+    private boolean mForceShowDismiss = false;
+
     public ScreenshotView(Context context) {
         this(context, null);
     }
@@ -196,6 +198,7 @@ public class ScreenshotView extends FrameLayout implements
         mCornerSizeX = mResources.getDimensionPixelSize(R.dimen.global_screenshot_x_scale);
         mDismissDeltaY = mResources.getDimensionPixelSize(
                 R.dimen.screenshot_dismissal_height_delta);
+        mForceShowDismiss = mResources.getBoolean(R.bool.always_show_screenshot_dismiss_botton);
 
         // standard material ease
         mFastOutSlowIn =
@@ -506,7 +509,7 @@ public class ScreenshotView extends FrameLayout implements
                 mScreenshotPreview.setScaleX(currentScale);
                 mScreenshotPreview.setScaleY(currentScale);
                 mScreenshotPreview.setVisibility(View.VISIBLE);
-                if (mAccessibilityManager.isEnabled()) {
+                if (mAccessibilityManager.isEnabled() || mForceShowDismiss) {
                     mDismissButton.setAlpha(0);
                     mDismissButton.setVisibility(View.VISIBLE);
                 }
@@ -895,7 +898,7 @@ public class ScreenshotView extends FrameLayout implements
         mScrollablePreview.setVisibility(View.GONE);
         mScrollingScrim.setVisibility(View.GONE);
 
-        if (mAccessibilityManager.isEnabled()) {
+        if (mAccessibilityManager.isEnabled() || mForceShowDismiss) {
             mDismissButton.setVisibility(View.VISIBLE);
         }
         mActionsContainer.setVisibility(View.VISIBLE);

omnirom/android_frameworks_base_new

packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotView.java
@@ -180,6 +180,8 @@ public class ScreenshotView extends FrameLayout implements
         QUICK_SHARE
     }
 
+    private boolean mForceShowDismiss = false;
+
     public ScreenshotView(Context context) {
         this(context, null);
     }
@@ -203,6 +205,8 @@ public class ScreenshotView extends FrameLayout implements
         mInteractiveBroadcastOption = options.toBundle();
 
         mFixedSize = mResources.getDimensionPixelSize(R.dimen.overlay_x_scale);
+
+        mForceShowDismiss = mResources.getBoolean(R.bool.always_show_screenshot_dismiss_button);
 
         // standard material ease
         mFastOutSlowIn =
@@ -544,7 +548,7 @@ public class ScreenshotView extends FrameLayout implements
                 mScreenshotPreview.setScaleX(currentScale);
                 mScreenshotPreview.setScaleY(currentScale);
                 mScreenshotPreview.setVisibility(View.VISIBLE);
-                if (mAccessibilityManager.isEnabled()) {
+                if (mAccessibilityManager.isEnabled() || mForceShowDismiss) {
                     mDismissButton.setAlpha(0);
                     mDismissButton.setVisibility(View.VISIBLE);
                 }
@@ -1005,7 +1009,7 @@ public class ScreenshotView extends FrameLayout implements
         mScrollablePreview.setVisibility(View.GONE);
         mScrollingScrim.setVisibility(View.GONE);
 
-        if (mAccessibilityManager.isEnabled()) {
+        if (mAccessibilityManager.isEnabled() || mForceShowDismiss) {
             mDismissButton.setVisibility(View.VISIBLE);
         }
         mActionsContainer.setVisibility(View.VISIBLE);