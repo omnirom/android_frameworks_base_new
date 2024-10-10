ba3b4fbda6eb75553a37d227442174315849614c
micky387 <mickaelsaibi@free.fr>
2024-10-10 17:05:59 -0400

    SystemUI: Update QS columns and tile features on each preference changes
    Change-Id: I16f690279e60355bfd6d2209be8b73a05e85d287

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
@@ -85,6 +85,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
     private final DumpManager mDumpManager;
     protected final ArrayList<TileRecord> mRecords = new ArrayList<>();
     protected boolean mShouldUseSplitNotificationShade;
+    private boolean shouldChangeTiles = false;
 
     @Nullable
     private Consumer<Boolean> mMediaVisibilityChangedListener;
@@ -328,7 +329,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
 
         // If we detected that the existing tiles are different than the requested tiles, clear them
         // and add the new tiles.
-        if (shouldChangeAll) {
+        if (shouldChangeAll || shouldChangeTiles) {
             for (QSPanelControllerBase.TileRecord record : mRecords) {
                 mView.removeTile(record);
                 record.tile.removeCallback(record.callback);
@@ -600,6 +601,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
         if (mView.getTileLayout() != null) {
             mView.getTileLayout().updateSettings();
             setTiles();
+            shouldChangeTiles = true;
         }
     }
 
